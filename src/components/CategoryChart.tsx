9f2f6742562b3c12600e1ffb4a13cf4e88261a78
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2025-11-06 15:00:51 +0100

    Add category chart

---
+++ src/components/CategoryChart.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+import { CategoryCount } from "../types";
+
+interface Props {
+  data: CategoryCount[];
+}
+
+const CategoryChart: React.FC<Props> = ({ data }) => {
+  return (
+    <div className="h-96 w-full">
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart
+          data={data}
+          margin={{ top: 20, right: 30, left: 20, bottom: 120 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis
+            dataKey="name"
+            angle={-45}
+            textAnchor="end"
+            height={100}
+            interval={0}
+            tick={{ fontSize: 12 }}
+          />
+          <YAxis />
+          <Tooltip />
+          <Bar dataKey="count" fill="#4F46E5" />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default CategoryChart;
